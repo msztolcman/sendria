6936e6f0a85ae16ceccdc662cd21d3399ef07c28
ThiefMaster <adrian@planetcoding.net>
2013-05-02 20:27:36 +0200

    REST endpoints to retrieve message & message part

--- maildump/web.py
+++ maildump/web.py
@@ -1,6 +1,7 @@
 import json
+from cStringIO import StringIO
 from datetime import datetime
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for, send_file
 from logbook import Logger
 from functools import wraps
 
@@ -42,9 +43,14 @@ def rest(f):
         if ret is None:
             response = jsonify()
             response.status_code = 204  # no content
+        elif isinstance(ret, app.response_class):
+            response = ret
         elif isinstance(ret, tuple):
-            # code, result_dict
-            response = jsonify(**ret[1])
+            # code, result_dict|msg_string
+            if isinstance(ret[1], basestring):
+                response = jsonify(msg=ret[1])
+            else:
+                response = jsonify(**ret[1])
             response.status_code = ret[0]
         else:
             response = jsonify(**ret)
@@ -74,6 +80,40 @@ def delete_messages():
 @rest
 def get_messages():
     # TODO: SSE/WebSocket support
+    lightweight = not bool_arg(request.args.get('full'))
     return {
-        'messages': db.get_messages(not bool_arg(request.args.get('full')))
+        'messages': db.get_messages(lightweight)
     }
+
+
+def _part_url(part):
+    return url_for('get_message_part', message_id=part['message_id'], cid=part['cid'])
+
+
+@app.route('/messages/<int:message_id>.json', methods=('GET',))
+@rest
+def get_message_info(message_id):
+    lightweight = not bool_arg(request.args.get('full'))
+    message = db.get_message(message_id, lightweight)
+    if not message:
+        return 404, 'message does not exist'
+    message['formats'] = ['source']
+    if db.message_has_plain(message_id):
+        message['formats'].append('plain')
+    if db.message_has_html(message_id):
+        message['formats'].append('html')
+    message['attachments'] = [dict(part, href=_part_url(part)) for part in db.get_message_attachments(message_id)]
+    return message
+
+
+@app.route('/messages/<int:message_id>/parts/<int:cid>', methods=('GET',))
+@rest
+def get_message_part(message_id, cid):
+    part = db.get_message_part_cid(message_id, cid)
+    if not part:
+        return 404, 'part does not exist'
+    io = StringIO(part['body'])
+    io.seek(0)
+    response = send_file(io, part['type'], part['is_attachment'], part['filename'])
+    response.charset = part['charset'] or 'utf-8'
+    return response
