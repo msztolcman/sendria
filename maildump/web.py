f3bdce7af0f7ba47d9f9be7978915162bc9130aa
ThiefMaster <adrian@planetcoding.net>
2013-05-02 19:37:55 +0200

    REST endpoint to terminate application

--- maildump/web.py
+++ maildump/web.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from logbook import Logger
+from functools import wraps
+
+import maildump
 
 
 app = Flask(__name__)
-app._logger = Logger(__name__)
+app._logger = log = Logger(__name__)
+
+
+def rest(f):
+    """Decorator for simple REST endpoints.
+
+    Functions must return one of these values:
+    - a dict to jsonify
+    - nothing for an empty 204 response
+    - a tuple containing a status code and a dict to jsonify
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        ret = f(*args, **kwargs)
+        if ret is None:
+            response = jsonify()
+            response.status_code = 204  # no content
+        elif isinstance(ret, tuple):
+            # code, result_dict
+            response = jsonify(**ret[1])
+            response.status_code = ret[0]
+        else:
+            response = jsonify(**ret)
+        return response
+    return wrapper
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/', methods=('DELETE',))
+@rest
+def terminate():
+    log.debug('Terminate request received')
+    maildump.stop()
