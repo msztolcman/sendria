79dde4d0d69f5be132acd763e53d810df37275bb
ThiefMaster <adrian@planetcoding.net>
2013-05-02 18:40:54 +0200

    Add dummy index page

--- maildump/web.py
+++ maildump/web.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from logbook import Logger
 
 
 app = Flask(__name__)
 app._logger = Logger(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
