350def2e15bfd18b6f900e1b86cd01b3476bd362
ThiefMaster <adrian@planetcoding.net>
2013-05-05 16:11:54 +0200

    Move utility functions into separate module

---
+++ maildump/util.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+from flask import current_app
+from functools import wraps
+
+
+def _json_default(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(repr(obj) + ' is not JSON serializable')
+
+
+def json_dumps(obj):
+    return json.dumps(obj, default=_json_default, indent=4)
+
+
+def jsonify(*args, **kwargs):
+    return current_app.response_class(json_dumps(dict(*args, **kwargs)), mimetype='application/json')
+
+
+def bool_arg(arg):
+    return arg in ('yes', 'true', '1')
+
+
+def rest(f):
+    """Decorator for simple REST endpoints.
+
+    Functions must return one of these values:
+    - a dict to jsonify
+    - nothing for an empty 204 response
+    - a tuple containing a status code and a dict to jsonify
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        ret = f(*args, **kwargs)
+        if ret is None:
+            response = jsonify()
+            response.status_code = 204  # no content
+        elif isinstance(ret, current_app.response_class):
+            response = ret
+        elif isinstance(ret, tuple):
+            # code, result_dict|msg_string
+            if isinstance(ret[1], basestring):
+                response = jsonify(msg=ret[1])
+            else:
+                response = jsonify(**ret[1])
+            response.status_code = ret[0]
+        else:
+            response = jsonify(**ret)
+        return response
+
+    return wrapper
--- maildump/web.py
+++ maildump/web.py
@@ -1,66 +1,19 @@
 import bs4
-import json
 import re
 from cStringIO import StringIO
-from datetime import datetime
-from flask import Flask, render_template, jsonify, request, url_for, send_file
+from flask import Flask, render_template, request, url_for, send_file
 from logbook import Logger
-from functools import wraps
 
 import maildump
 import maildump.db as db
+from maildump.util import rest, bool_arg
 
-
-app = Flask(__name__)
-app._logger = log = Logger(__name__)
 
 RE_CID = re.compile(r'(?P<replace>cid:(?P<cid>.+))')
 RE_CID_URL = re.compile(r'url\(\s*(?P<quote>["\']?)(?P<replace>cid:(?P<cid>[^\\\')]+))(?P=quote)\s*\)')
 
-
-def _json_default(obj):
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise TypeError(repr(obj) + ' is not JSON serializable')
-
-
-def jsonify(*args, **kwargs):
-    return app.response_class(json.dumps(dict(*args, **kwargs), default=_json_default, indent=4),
-                              mimetype='application/json')
-
-
-def bool_arg(arg):
-    return arg in ('yes', 'true', '1')
-
-
-def rest(f):
-    """Decorator for simple REST endpoints.
-
-    Functions must return one of these values:
-    - a dict to jsonify
-    - nothing for an empty 204 response
-    - a tuple containing a status code and a dict to jsonify
-    """
-
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        ret = f(*args, **kwargs)
-        if ret is None:
-            response = jsonify()
-            response.status_code = 204  # no content
-        elif isinstance(ret, app.response_class):
-            response = ret
-        elif isinstance(ret, tuple):
-            # code, result_dict|msg_string
-            if isinstance(ret[1], basestring):
-                response = jsonify(msg=ret[1])
-            else:
-                response = jsonify(**ret[1])
-            response.status_code = ret[0]
-        else:
-            response = jsonify(**ret)
-        return response
-    return wrapper
+app = Flask(__name__)
+app._logger = log = Logger(__name__)
 
 
 @app.route('/')
