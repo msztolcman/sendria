0cabe8c48a0e086f11d9fc8567ca0097d73d2c05
ThiefMaster <adrian@planetcoding.net>
2013-05-09 18:51:04 +0200

    Add handlers for quit/clear buttons

--- maildump/static/maildump.js
+++ maildump/static/maildump.js
@@ -12,5 +12,37 @@
                 $(document).off('.resizer');
             });
         });
+
+        function restCall(method, path) {
+            return $.ajax({
+                url: path,
+                type: method
+            }).fail(function(xhr, status, error) {
+                alert('REST call failed: ' + method + ' ' + path + '\nStatus: ' + status + '\nError: ' + error);
+            });
+        }
+
+        // Top nav actions
+        $('nav.app .quit a').on('click', function(e) {
+            e.preventDefault();
+            if(!confirm('Do you really want to terminate the MailDump application?')) {
+                return;
+            }
+            restCall('DELETE', '/').done(function() {
+                alert('Terminated.');
+                // TODO: Remove this message after we handle websocket events
+            });
+        });
+
+        $('nav.app .clear a').on('click', function(e) {
+            e.preventDefault();
+            if (!confirm('Do you really want to delete all messages?')) {
+                return;
+            }
+            restCall('DELETE', '/messages/').done(function() {
+                alert('Deleted all messages');
+                // TODO: Remove this message after we handle websocket events
+            });
+        });
     });
 })(jQuery);
