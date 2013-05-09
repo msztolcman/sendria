b93aa87681b861afa6f08f7e87fb0476d3ca4b52
ThiefMaster <adrian@planetcoding.net>
2013-05-09 18:06:19 +0200

    Add layout (mailcatcher ripoff for now)

--- maildump/static/maildump.js
+++ maildump/static/maildump.js
@@ -1,3 +1,16 @@
 (function($) {
-
+    $(document).ready(function() {
+        $('.resizer').on('mousedown', function(e) {
+            var $this = $(this);
+            var target = $this.data('sibling') == 'prev' ? $this.prev() : $this.next();
+            e.preventDefault();
+            $(document).on('mousemove.resizer',function(e) {
+                e.preventDefault();
+                target.css('height', e.clientY - target.offset().top);
+            }).on('mouseup.resizer', function(e) {
+                e.preventDefault();
+                $(document).off('.resizer');
+            });
+        });
+    });
 })(jQuery);
