037c5ae30f29040c2d16adb25a936f872603d6db
ThiefMaster <adrian@planetcoding.net>
2013-05-14 18:45:58 +0200

    Add setup.py and a proper console script

---
+++ setup.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='maildump',
+    version='0.1',
+    description='A SMTP server that makes all received mails via a web interface and REST API.',
+    long_description=readme,
+    url='https://github.com/ThiefMaster/maildump',
+    download_url='https://github.com/ThiefMaster/maildump',
+    author=u'Adrian Mönnich',
+    author_email='adrian@planetcoding.net',
+    license='MIT',
+    zip_safe=False,
+    include_package_data=True,
+    packages=('maildump',),
+    entry_points={
+        'console_scripts': [
+            'maildump = maildump.__main__',
+        ],
+    },
+    install_requires=requirements
+)
