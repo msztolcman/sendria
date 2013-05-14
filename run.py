5e89efe833adcb7747655bb2d1d9d3c260b1106b
ThiefMaster <adrian@planetcoding.net>
2013-05-14 19:23:16 +0200

    Add helper script to run without installing

---
+++ run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# This is a small utility to run maildump during development without installing it first.
+
+from maildump_runner import main
+
+if __name__ == '__main__':
+    main()
