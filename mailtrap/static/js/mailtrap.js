82f6c18fa8af607b93ae076a1a8038c55b56c737
Marcin Sztolcman <marcin@urzenia.net>
2020-07-25 21:43:57 +0200

    exatract websocket url to variable

--- mailtrap/static/js/mailtrap.js
+++ mailtrap/static/js/mailtrap.js
@@ -126,10 +126,11 @@
 
         // Real-time updates
         function wsConnect() {
+            var wsUrl = window.location.host + '/ws';
             try {
-                var socket = new WebSocket('ws://' + window.location.host + '/ws');
+                var socket = new WebSocket('ws://' + wsUrl);
             } catch (err) {
-                var socket = new WebSocket('wss://' + window.location.host + '/ws');
+                var socket = new WebSocket('wss://' + wsUrl);
             }
             var terminating = false;
             window.onbeforeunload = function () {
